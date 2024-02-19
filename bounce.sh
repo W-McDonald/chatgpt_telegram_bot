6bcaf3bc9a4c15e3f5bead391bbf2678ffecb731
Weston McDonald <westonjmcdonald@gmail.com>
2024-02-19 22:07:51 +0000

    updating shell script to work smoother and include better logging, installing missing packages

--- bounce.sh
+++ bounce.sh
@@ -8,16 +8,22 @@ LOG_DIR="$BASE_DIR/output_logs"
 # Fetching the PID of the python script
 pid=$(ps -ef | grep $SCRIPT_DIR/main.py | grep -v grep | awk '{print $2}')
 
+# Set the output log filename without including the LOG_DIR path
+log_filename="output_$(date +'%Y%m%d').log"
+
+# Logging function to write a message to the log file
+log_message() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - ${1}" >> "$LOG_DIR/$log_filename"
+}
+
 # Killing the PID
 if [ -n "$pid" ]; then
     echo "Killing process with PID: $pid"
     kill -9 $pid
+    log_message "SYSTEM: Process stopped gracefully by $(basename $0)"
 else
     echo "No process found!"
 fi
-
-# Set the output log filename without including the LOG_DIR path
-log_filename="output_$(date +'%Y%m%d').log"
 
 # Check if the log file exists for today's date
 if [ -f "$LOG_DIR/$log_filename" ]; then
@@ -27,8 +33,9 @@ else
     touch "$LOG_DIR/$log_filename"
 fi
 
-
 # Restarting the script with a nohup, in the background
 nohup python $SCRIPT_DIR/main.py >> "$LOG_DIR/$log_filename" 2>&1 &
 new_pid=$!
 echo "Process started with PID: $new_pid"
+log_message "SYSTEM: Process started by $(basename $0) with PID: $new_pid"
+
